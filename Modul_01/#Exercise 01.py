12747d5d3fcfb73f534179e241462304e945a69a
NurrafiNarendraji <127179019+NurrafiNarendraji@users.noreply.github.com>
2023-03-14 11:18:10 +0700

    mengumpulkan tugas

---
+++ Modul_01/#Exercise 01.py
@@ -0,0 +1,12 @@
+#Exercise 01
+Nama = "Nurrafi Narendraji"
+NIM = "I0322096"
+Visualisasi = "Saya berasal dari kota Surakarta,Kecamatan Jebres,Kelurahan Kepatihan Kulon,tempat tinggal saya termasuk dekat dengan universitas sebelas maret"
+Harapan = "Harapan saya setelah menjadi lulusan TI adalah menjadi harapan bangsa indonesia yang agung dan membanggakan orang tua"
+
+
+print("Assalamualaikum wr wb, Halo semuanya")
+print("Perkenalkan namaku",Nama)
+print("NIM saya",NIM)
+print(Visualisasi)
+print(Harapan)
